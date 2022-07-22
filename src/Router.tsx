acf29546769c10794db38e1121d2a702e0c9de8b
Jo hyebin <hyebinjo91@gmail.com>
2022-07-22 20:44:21 +0900

    Feat: ApplicantsList컴포넌트 (#4)
    * init: 초기세팅 수정
    
    * chore: Gender타입 추가
    
    * feat: ApplicantsList.tsx
    
    * feat: Pagination 기능완성
    
    * style: pagination 스타일링
    
    * style: Table 스타일링
    
    * chore: Applicant인터페이스에 order추가
    
    * feat: order표시
    
    * feat: ApplicantsList.tsx에 data샘플 적용
    
    Co-authored-by: seongjin <77876601+ysjkof@users.noreply.github.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,13 @@ import ApplyStatus from './pages/ApplyStatus';
 import Registration from './pages/Registration';
 import TestPage from './pages/TestPage';
 
+
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestPage />} />
-        <Route path="/Application" element={<ApplyStatus />} />
+        <Route path="/" element={<Registration />} />
+        <Route path="/applicants" element={<ApplyStatus />} />
       </Routes>
     </BrowserRouter>
   );
--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,16 +1,20 @@
 export type TransportationTypes = '자가용' | '버스' | '지하철' | '택시' | '자전거' | 'KTX/기차' | '전동킥보드' | '도보';
+export type Gender = '남' | '여';
+
 
 export interface Applicant {
   round: number;
   id: number;
   date: Date;
   name: string;
+  gender: Gender;
   birth: Date;
   contact: string;
   email: string;
   transportation: TransportationTypes;
   address: string;
   accepted: boolean;
+  order?: number;
 }
 export interface responseType {
   config:object,
